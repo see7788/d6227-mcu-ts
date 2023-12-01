f7f36db64af0e2c9129cedf29244dffbede18912
see7788 <kitchens@qq.com>
2023-12-02 02:13:25 +0800

    11111111111111

--- src/cpp_mcu00_web/cppUse.ts
+++ src/cpp_mcu00_web/cppUse.ts
@@ -5,12 +5,13 @@ import path from "node:path"
 import { fileURLToPath } from "node:url"
 import fs from "node:fs"
 const { outDir } = Object.fromEntries(process.argv.slice(2).map(v => v.replace("--", "")).map(v => v.split("="))) as { outDir: string };
-function createFile(config: any,i18n:any) {
+function createFile(config: any, i18n: any) {
     try {
         echo = { ...echo, data: JSON.stringify(config, null, 2) };
         fs.mkdirSync(outDir, { recursive: true });
         fs.writeFileSync(path.resolve(outDir, "config.json"), JSON.stringify(config), { flag: 'w' });
         fs.writeFileSync(path.resolve(outDir, "i18n.json"), JSON.stringify(i18n), { flag: 'w' });
+        process.exit(0)
     } catch (e) {
         console.error("js console", { ...echo, e });
         process.exit(1);
@@ -20,8 +21,15 @@ let echo: any = { outDir };
 if (!outDir) {
     console.error({ ...echo, e: "!outDir" });
     process.exit(1)
-} else  {
-    createFile(mcu00.configBase,mcu00.i18n)
-    console.log("js console success", echo);
-    process.exit(0)
+} else {
+    try {
+        echo = { ...echo, data: JSON.stringify(mcu00, null, 2) };
+        fs.mkdirSync(outDir, { recursive: true });
+        fs.writeFileSync(path.resolve(outDir, "config.json"), JSON.stringify(mcu00.configBase), { flag: 'w' });
+        fs.writeFileSync(path.resolve(outDir, "i18n.json"), JSON.stringify(mcu00.i18n), { flag: 'w' });
+        process.exit(0)
+    } catch (e) {
+        console.error("js console", { ...echo, e });
+        process.exit(1);
+    }
 }
